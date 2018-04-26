5bfa1ab64b2128e8df026566e3736dafa6ce984d
liupeng <624298206@qq.com>
2018-04-26 14:53:02 +0800

    定制ajax

--- ajax.js
+++ ajax.js
@@ -1,49 +1,39 @@
-// axios get
-axios.get('/user', {
-    params: {
-      ID: 12345
+/* 针对axios设置常用请求方式
+    path 请求地址，必填
+    其余参数通过对象的解构赋值，至少传递一个空对象
+    type 请求类型，get,post,file 三种方式，默认为get，file为上传文件时的请求
+    method 请求方式
+    data 请求数据
+    success 请求完成时回调
+    failure 请求失败时回调
+ */
+
+export default function(path, { type = 'GET', method = 'GET', data = '', success, failure = '' }) {
+    let options {
+        method: method,
+        url: path,
+        data: data,
     }
-  })
-  .then(function (response) {
-    console.log(response);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-  
-// axios post
-axios({
-    method: "post",
-    url: userlogin,
-    data: {
-
-    },
-    transformRequest: [
-        function(data) {
-            var ret = ''
-            for (var it in data) {
-                ret += encodeURIComponent(it) +
-                    '=' +
-                    encodeURIComponent(data[it]) +
-                    '&'
+    if (type == 'post') {
+        options.transformRequest = [
+            function(data) {
+                var ret = ''
+                for (var it in data) {
+                    ret += encodeURIComponent(it) +
+                        '=' +
+                        encodeURIComponent(data[it]) +
+                        '&'
+                }
+                return ret
             }
-            return ret
-        }
-    ]
-}).then(function(res) {
-
-})
-
-
-// 上传formdata
-var formdata = new FormData();
-formdata.append("type", "1");
-axios.post(queren, formdata, {
-    headers: {
-        'Content-Type': 'multiple/form-data'
+        ]
     }
-}).then(function(res) {
-    console.log(res);
-    location.href = "/";
-});
+    if (type == 'file') {
+        options.headers = { 'Content-Type': 'multiple/form-data' }
+    }
+    axios(options).then(function(res) {
+        success(res)
+    }).catch(function(err) {
+        if (failure) { failure(res) }
+    })
+}
--- common.js
+++ common.js
@@ -1,3 +1,6 @@
+/* 
+    将数字转为每三位间隔样式 
+*/
 function toK(num) {
     var num = (num || 0).toString(),
         result = '';
@@ -8,7 +11,12 @@ function toK(num) {
     if (num) { result = num + result; }
     return result;
 }
-// 获取随机数组成的数组函数，length，min，max
+
+/* 获取随机数组成的数组函数
+    min 随机最小值
+    max 随机最大值
+    length 随机个数
+ */
 function random(min, max, length) {
     if (arguments.length > 2) {
         var arr = []
@@ -19,83 +27,15 @@ function random(min, max, length) {
     }
     return Math.ceil(Math.random() * (max - min) + min)
 }
-// 获取时间段
-function adTime(date) {
-    var time = date.getTime()
-    var day = date.getDay();
-    var t1 = new Date();
-    var t2 = new Date();
-    var t3 = new Date();
-    var t4 = new Date();
-    var t5 = new Date();
-    var t6 = new Date();
-    var t7 = new Date();
-    var t8 = new Date();
-    var t9 = new Date();
-    var t10 = new Date();
-    var t11 = new Date();
-    var t12 = new Date();
-    var t13 = new Date();
-    var t14 = new Date();
-    var t15 = new Date();
-    var t16 = new Date();
-    var timeArr = [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16];
-    var strArr = [];
-    var htmlStr = "";
-    if (day < 6) {
-        var addTime = (6 - day) * 86400000;
-        var newTime = time + addTime;
-        for (var i = 0; i < timeArr.length; i++) {
-            timeArr[i].setTime(newTime);
-        }
-    }
 
-    for (var i = 0; i < timeArr.length; i++) {
-        if (i > 0) {
-            timeArr[i].setTime(timeArr[i - 1].getTime() + (7 * 86400000))
-        }
-        // console.log(timeArr[i]);
-        getD = timeArr[i].getDate() < 10 ? "0" + timeArr[i].getDate() : timeArr[i].getDate();
-        // console.log(timeArr[i].getDate());
-        strArr[i] = timeArr[i].getMonth() + 1 + '.' + getD;
-
-    }
-
-    return strArr;
-
-}
-adTime(new Date())
-
-// 遍历loc
-for (var i = 0; i < localStorage.length; i++) {
-    if (localStorage.key(i).slice(0, 4) == 'myad') {
-        console.log(localStorage.key(i));
-    }
-}
-// 获取日期函数,dataArr为当前日期后30天日期数组
-function GetDateStr(AddDayCount) {
-    var dd = new Date();
-    dd.setDate(dd.getDate() + AddDayCount); //获取AddDayCount天后的日期  
-    var m = (dd.getMonth() + 1) < 10 ? "0" + (dd.getMonth() + 1) : (dd.getMonth() + 1); //获取当前月份的日期，不足10补0  
-    var d = dd.getDate() < 10 ? "0" + dd.getDate() : dd.getDate(); //获取当前几号，不足10补0  
-    return +m + "-" + d;
-}
-
-
-// 获取当前7天
-var myDate = new Date(); //获取今天日期
-myDate.setDate(myDate.getDate() - 7);
-var dateArray = [];
-var dateTemp;
-var flag = 1;
-for (var i = 0; i < 7; i++) {
-    dateTemp = (myDate.getMonth() + 1) + "-" + myDate.getDate();
-    dateArray.push(dateTemp);
-    myDate.setDate(myDate.getDate() + flag);
-}
-
-// 读取上传图像
-function fileRead(event, size, img, date) {
+/* 获取上传图像
+    event 获取入口input
+    size 尺寸控制
+    img 渲染img
+    date 图像储存容器
+    error 错误提示
+ */
+function fileRead(event, size, img, date, error) {
     var ref = /image\/(jpeg||png)/i;
     var file = event.target.files[0];
 
@@ -110,7 +50,6 @@ function fileRead(event, size, img, date) {
         date = file
     } else if (file.length < 1) {
         error = "请选择文件!";
-
     } else {
         error = "请选择" + size / 1000 + "kb以下的jpg/png文件!";
 
