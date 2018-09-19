822e9e54905649ffc0ed741fcd6de09b3331c7cf
liupeng <624298206@qq.com>
2018-09-19 10:52:59 +0800

    up

--- rem.js
+++ rem.js
@@ -29,16 +29,4 @@
     }
   })
 
-  // detect 0.5px supports
-  if (dpr >= 2) {
-    var fakeBody = document.createElement('body')
-    var testElement = document.createElement('div')
-    testElement.style.border = '.5px solid transparent'
-    fakeBody.appendChild(testElement)
-    docEl.appendChild(fakeBody)
-    if (testElement.offsetHeight === 1) {
-      docEl.classList.add('hairlines')
-    }
-    docEl.removeChild(fakeBody)
-  }
 }(window, document))
