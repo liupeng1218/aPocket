dd7115389b60e3ef978c74fb742eeb16e699f4e9
liupeng <624298206@qq.com>
2018-12-26 15:05:43 +0800

    up

--- util.js
+++ util.js
@@ -55,3 +55,17 @@ function fileRead(event, size, img, date, error) {
 
     }
 }
+
+/*
+    时间戳格式化
+*/
+function dateFormat(date) {
+    var now = new Date(sj * 1000);
+    var year = now.getFullYear();
+    var month = now.getMonth() + 1;
+    var date = now.getDate();
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+    var second = now.getSeconds();
+    return year + "-" + month + "-" + date + "   " + hour + ":" + minute + ":" + second;
+}
