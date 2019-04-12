f608f067bb7f28c996d33c75dbfdeeecec13d704
lp <liupeng0124@qq.com>
2019-04-12 18:02:02 +0800

    up

--- ajax.js
+++ ajax.js
@@ -1,81 +1,64 @@
-import axios from 'axios'
-import ui from '@/util/msg'
+import axios from "axios";
+
 // create an axios instance
 const ajax = axios.create({
-    timeout: 10000 // request timeout
-})
+  timeout: 10000 // request timeout
+});
 
 // request interceptor
 ajax.interceptors.request.use(
-    config => {
-        // Do something before request is sent
-        return config
-    },
-    error => {
-        // Do something with request error
-
-        console.log(error) // for debug
-        Promise.reject(error)
-    }
-)
-
-// // response interceptor
-ajax.interceptors.response.use(
-    
-    response => {
-        // console.log(response)
-        if (response.data.ret != 200) {
-            ui.tips(response.data.msg)
+  config => {
+    // Do something before request is sent
+    // console.log(config);
+    if (config.method == "post" && !config.isUpdate) {
+      config.transformRequest = [
+        function(data) {
+          var ret = "";
+          for (var it in data) {
+            ret +=
+              encodeURIComponent(it) + "=" + encodeURIComponent(data[it]) + "&";
+          }
+          return ret;
         }
-        return response
-    },
-
-    error => {
-        console.log('err' + error) // for debug
-        ui.tips(error)
-        return Promise.reject(error)
+      ];
     }
-)
+    if (config.isUpdate) {
+      config.headers = { "Content-Type": "multiple/form-data" };
+      config.method = "post";
+    }
+    return config;
+  },
+  error => {
+    // Do something with request error
+
+    console.log(error); // for debug
+    Promise.reject(error);
+  }
+);
+
+// response interceptor
+ajax.interceptors.response.use(
+  response => {
+    console.log(response)
+    return response;
+  },
+
+  error => {
+    console.log("err" + error); // for debug
+
+    return Promise.reject(error);
+  }
+);
 
 
-/* 针对axios设置常用请求方式
-    path 请求地址，必填
-    其余参数通过对象的解构赋值，至少传递一个空对象
-    type 请求类型，get,post,file 三种方式，默认为get，file为上传文件时的请求
-    method 请求方式
-    data 请求数据
-    success 请求完成时回调
-    failure 请求失败时回调
- */
-
-export default function(path, { type = 'GET', method = 'GET', data = '', success, failure = '' }) {
-    let options = {
-        method: method,
-        url: path,
-        data: data,
-    }
-    if (type == 'post') {
-        options.transformRequest = [
-            function(data) {
-                var ret = ''
-                for (var it in data) {
-                    ret += encodeURIComponent(it) +
-                        '=' +
-                        encodeURIComponent(data[it]) +
-                        '&'
-                }
-                return ret
-            }
-        ]
-        options.method = 'POST'
-    }
-    if (type == 'file') {
-        options.headers = { 'Content-Type': 'multiple/form-data' }
-        options.method = 'POST'
-    }
-    ajax(options).then(function(res) {
-        success(res)
-    }).catch(function(err) {
-        if (failure) { failure(res) }
-    })
+export default function(url, options) {
+  return new Promise((resolve, reject) => {
+    ajax(url, options)
+      .then(response => {
+        resolve(response.data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
 }
--- util.js
+++ util.js
@@ -1,118 +1,124 @@
-
 /* 获取随机数组成的数组函数
     min 随机最小值
     max 随机最大值
     length 随机个数
  */
 function random(min, max, length) {
-    if (arguments.length > 2) {
-        var arr = []
-        for (var i = 0; i < length; i++) {
-            arr.push(Math.ceil(Math.random() * (max - min) + min))
-        }
-        return arr;
+  if (arguments.length > 2) {
+    var arr = [];
+    for (var i = 0; i < length; i++) {
+      arr.push(Math.ceil(Math.random() * (max - min) + min));
     }
-    return Math.ceil(Math.random() * (max - min) + min)
+    return arr;
+  }
+  return Math.ceil(Math.random() * (max - min) + min);
 }
 
 /* 获取上传图像
     event 获取入口input
     size 尺寸控制
-    img 渲染img
+    img img容器
     date 图像储存容器
     error 错误提示
  */
-function fileRead(event, size, img, date, error) {
-    var ref = /image\/(jpeg||png)/i;
-    var file = event.target.files[0];
+function fileRead(event, size = 2048000, img, date = undefined, error = "") {
+  var ref = /image\/(jpeg||png)/i;
+  var file = event.target.files[0];
 
-    if (ref.test(file.type) && file.size < size) {
-        var reader = new FileReader();
-        reader.onload = (function (file) {
-            return function (e) {
-                img[url] = this.result;
-            };
-        })(file);
-        reader.readAsDataURL(file);
-        date = file
-    } else if (file.length < 1) {
-        error = "请选择文件!";
-    } else {
-        error = "请选择" + size / 1000 + "kb以下的jpg/png文件!";
-
-    }
+  if (ref.test(file.type) && file.size < size) {
+    var reader = new FileReader();
+    reader.onload = (function(file) {
+      return function(e) {
+        img[url] = this.result;
+      };
+    })(file);
+    reader.readAsDataURL(file);
+    date = file;
+  } else if (file.length < 1) {
+    error = "请选择文件!";
+  } else {
+    error = "请选择" + size / 1000 + "kb以下的jpg/png文件!";
+  }
+  return {
+    file: date,
+    error: error
+  };
 }
 
 /*
     时间戳格式化
 */
 function dateFormat(date) {
-    var now = new Date(sj * 1000);
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var date = now.getDate();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    return year + "-" + month + "-" + date + "   " + hour + ":" + minute + ":" + second;
+  var now = new Date(sj * 1000);
+  var year = now.getFullYear();
+  var month = now.getMonth() + 1;
+  var date = now.getDate();
+  var hour = now.getHours();
+  var minute = now.getMinutes();
+  var second = now.getSeconds();
+  return (
+    year + "-" + month + "-" + date + "   " + hour + ":" + minute + ":" + second
+  );
 }
 
 /*
     获取浏览器agent
 */
 function IEVersion() {
-    var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串  
-    var isIE = userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器  
-    var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器  
-    var isIE11 = userAgent.indexOf('Trident') > -1 && userAgent.indexOf("rv:11.0") > -1;
-    if (isIE) {
-        var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
-        reIE.test(userAgent);
-        var fIEVersion = parseFloat(RegExp["$1"]);
-        if (fIEVersion == 7) {
-            return 7;
-        } else if (fIEVersion == 8) {
-            return 8;
-        } else if (fIEVersion == 9) {
-            return 9;
-        } else if (fIEVersion == 10) {
-            return 10;
-        } else {
-            return 6; //IE版本<=7
-        }
-    } else if (isEdge) {
-        return 'edge'; //edge
-    } else if (isIE11) {
-        return 11; //IE11  
+  var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+  var isIE =
+    userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1; //判断是否IE<11浏览器
+  var isEdge = userAgent.indexOf("Edge") > -1 && !isIE; //判断是否IE的Edge浏览器
+  var isIE11 =
+    userAgent.indexOf("Trident") > -1 && userAgent.indexOf("rv:11.0") > -1;
+  if (isIE) {
+    var reIE = new RegExp("MSIE (\\d+\\.\\d+);");
+    reIE.test(userAgent);
+    var fIEVersion = parseFloat(RegExp["$1"]);
+    if (fIEVersion == 7) {
+      return 7;
+    } else if (fIEVersion == 8) {
+      return 8;
+    } else if (fIEVersion == 9) {
+      return 9;
+    } else if (fIEVersion == 10) {
+      return 10;
     } else {
-        return -1; //不是ie浏览器
+      return 6; //IE版本<=7
     }
+  } else if (isEdge) {
+    return "edge"; //edge
+  } else if (isIE11) {
+    return 11; //IE11
+  } else {
+    return -1; //不是ie浏览器
+  }
 }
 
 /*
     解构 url query
 */
 function parseUrl(url) {
-    if (url) {
-        url = url.substr(url.indexOf("?") + 1);
-    }
+  if (url) {
+    url = url.substr(url.indexOf("?") + 1);
+  }
 
-    if (url.length < 1) {
-        return false
-    }
-    var result = {}, // save name value
-        queryString = url || location.search.substring(1),
-        reg = /([^&=]+)=([^&]*)/g,
-        item
-    while (item = reg.exec(queryString)) {
-        result[decodeURIComponent(item[1])] = decodeURIComponent(item[2]);
-    }
-    return result;
+  if (url.length < 1) {
+    return false;
+  }
+  var result = {}, // save name value
+    queryString = url || location.search.substring(1),
+    reg = /([^&=]+)=([^&]*)/g,
+    item;
+  while ((item = reg.exec(queryString))) {
+    result[decodeURIComponent(item[1])] = decodeURIComponent(item[2]);
+  }
+  return result;
 }
 export default {
-    random: random,
-    fileRead: fileRead,
-    dateFormat: dateFormat,
-    IEVersion: IEVersion,
-    parseUrl: parseUrl
-}
+  random: random,
+  fileRead: fileRead,
+  dateFormat: dateFormat,
+  IEVersion: IEVersion,
+  parseUrl: parseUrl
+};
