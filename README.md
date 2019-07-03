5202bfea066a3379f1a7a1a1e8b7d6b4be15bb34
liupeng <624298206@qq.com>
2019-07-03 11:14:39 +0800

    updata 2.0

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-## aPocket
+# 维护常用的类库和工具类函数
 
-aPocket 记录常用的功能函数及模块
+## rem
 
-### 结构
-1. ajax: axios常用的get,post结构
-2. common：常用的公共函数
+基于 淘宝rem解决方案 [flexible](https://github.com/amfe/lib-flexible) 定制的 移动端rem解决方案
+
+## util
+
+定义常用的工具方法
+
 
--- util.js
+++ util.js
@@ -1,71 +1,74 @@
-/* 
-    将数字转为每三位间隔样式 
-*/
-function toK(num) {
-    var num = (num || 0).toString(),
-        result = '';
+export default (function(window) {
+  /**
+   * 将数字转为每三位间隔样式
+   *
+   * @param {Number} num
+   * @returns
+   */
+  function toK(num) {
+    num = (num || 0).toString()
+    var result = ''
     while (num.length > 3) {
-        result = ',' + num.slice(-3) + result;
-        num = num.slice(0, num.length - 3);
+      result = ',' + num.slice(-3) + result
+      num = num.slice(0, num.length - 3)
     }
-    if (num) { result = num + result; }
-    return result;
-}
+    if (num) { result = num + result }
+    return result
+  }
 
-/* 获取随机数组成的数组函数
-    min 随机最小值
-    max 随机最大值
-    length 随机个数
- */
-function random(min, max, length) {
+  /**
+   * 获取随机数组成的数组函数
+   *
+   * @param {Number} min
+   * @param {Number} max
+   * @param {Number} length
+   * @returns
+   */
+  function random(min, max, length) {
     if (arguments.length > 2) {
-        var arr = []
-        for (var i = 0; i < length; i++) {
-            arr.push(Math.ceil(Math.random() * (max - min) + min))
-        }
-        return arr;
+      var arr = []
+      for (var i = 0; i < length; i++) {
+        arr.push(Math.ceil(Math.random() * (max - min) + min))
+      }
+      return arr
     }
     return Math.ceil(Math.random() * (max - min) + min)
-}
+  }
 
-/* 获取上传图像
-    event 获取入口input
-    size 尺寸控制
-    img 渲染img
-    date 图像储存容器
-    error 错误提示
- */
-function fileRead(event, size, img, date, error) {
-    var ref = /image\/(jpeg||png)/i;
-    var file = event.target.files[0];
+  /**
+   * 获取上传图像
+   *
+   * @param {DOM} 入口dom
+   * @param {Number} size
+   * @param {Object} 存贮渲染图像的对象
+   * @param {Object} 存储上传文件的对象
+   * @param {String} 错误提示
+   */
+  function fileRead(dom, size, img, date, error) {
+    var ref = /image\/(jpeg||png)/i
+    var file = dom.target.files[0]
 
     if (ref.test(file.type) && file.size < size) {
-        var reader = new FileReader();
-        reader.onload = (function(file) {
-            return function(e) {
-                img[url] = this.result;
-            };
-        })(file);
-        reader.readAsDataURL(file);
-        date = file
+      var reader = new FileReader()
+      reader.onload = (function(file) {
+        return function(e) {
+          img['url'] = this.result
+        }
+      })(file)
+      reader.readAsDataURL(file)
+      date = file
     } else if (file.length < 1) {
-        error = "请选择文件!";
+      error = '请选择文件!'
     } else {
-        error = "请选择" + size / 1000 + "kb以下的jpg/png文件!";
-
+      error = '请选择' + size / 1000 + 'kb以下的jpg/png文件!'
     }
-}
+  }
 
-/*
-    时间戳格式化
-*/
-function dateFormat(date) {
-    var now = new Date(sj * 1000);
-    var year = now.getFullYear();
-    var month = now.getMonth() + 1;
-    var date = now.getDate();
-    var hour = now.getHours();
-    var minute = now.getMinutes();
-    var second = now.getSeconds();
-    return year + "-" + month + "-" + date + "   " + hour + ":" + minute + ":" + second;
-}
+  var Uutil = {
+    toK: toK,
+    random: random,
+    fileRead: fileRead
+  }
+  window.$Uutil = Uutil
+  return Uutil
+})(window)
