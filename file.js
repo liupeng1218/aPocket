786ba820b19d8f364ab2bbc66466ec9dc78ad97e
liupeng <624298206@qq.com>
2021-07-08 15:25:03 +0800

    up

--- file.js
+++ file.js
@@ -11,3 +11,26 @@ export function fileExport(res) {
   URL.revokeObjectURL(elink.href) // 释放URL 对象
   document.body.removeChild(elink)
 }
+
+
+
+export function downloadRes(response) {
+  const filename = response.headers['content-disposition'].match(
+    /filename=(.*);/
+  )[1]
+  // 首先要创建一个 Blob 对象（表示不可变、原始数据的类文件对象）
+  const blob = new Blob([response.data])
+  if (typeof window.navigator.msSaveBlob !== 'undefined') {
+  // 兼容IE，window.navigator.msSaveBlob：以本地方式保存文件
+    window.navigator.msSaveBlob(blob, decodeURI(filename))
+  } else {
+    const elink = document.createElement('a') // 创建一个<a>标签
+    elink.style.display = 'none' // 隐藏标签
+    elink.href = window.URL.createObjectURL(blob) // 配置href，指向本地文件的内存地址    
+    elink.download = filename
+    document.body.appendChild(elink)
+    elink.click()
+    URL.revokeObjectURL(elink.href) // 释放URL 对象
+    document.body.removeChild(elink) // 移除<a>标签
+  }
+}
